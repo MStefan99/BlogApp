a705731685169314a3063b1211d30d16ea218ffc
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2019-02-09 10:01:06 +0200

    migrating from sqlite to mysql

--- blog_app.py
+++ blog_app.py
@@ -3,18 +3,68 @@ from passlib.hash import pbkdf2_sha512
 import datetime
 import random
 import string
-import sqlite3
+import mysql.connector
 
 app = Flask(__name__)
-DATABASE = './database/db.sqlite'
+
+DATABASE = mysql.connector.connect(user='flask', password='MySQLPassword',
+                                   database='Blog', auth_plugin='mysql_native_password')
+CURSOR = DATABASE.cursor()
+CURSOR.execute('''create table if not exists posts
+(
+    ID int auto_increment
+        primary key,
+    Title varchar(256) not null,
+    Tagline varchar(512) not null,
+    Image varchar(512) null,
+    Splash varchar(512) null,
+    Theme_Color char(7) null,
+    Link varchar(512) not null,
+    Author varchar(256) not null,
+    Content text not null,
+    Date datetime not null,
+    Tags text null
+);
+
+create table if not exists users
+(
+    Username varchar(255) not null,
+    Password varchar(255) not null,
+    CookieID char(255) not null,
+    Email varchar(255) not null,
+    ID int auto_increment
+        primary key,
+    constraint users_CookieID_uindex
+        unique (CookieID),
+    constraint users_Email_uindex
+        unique (Email),
+    constraint users_Username_uindex
+        unique (Username)
+);
+
+create table if not exists favourites
+(
+    User_ID int not null,
+    Post_ID int not null,
+    Date_Added datetime not null,
+    constraint Favourites_Post_ID_uindex
+        unique (Post_ID),
+    constraint Favourites_posts_ID_fk
+        foreign key (Post_ID) references posts (ID),
+    constraint Favourites_users_ID_fk
+        foreign key (User_ID) references users (ID)
+);
+
+alter table favourites
+    add primary key (Post_ID);
+    ''', multi=True)
 
 
 @app.route('/')
 def hello_world():
     if request.cookies.get('MSTID'):
-        database = sqlite3.connect(DATABASE)
-        c = database.cursor()
-        users = c.execute('SELECT * from Users').fetchall()
+        CURSOR.execute('SELECT * from Users')
+        users = CURSOR.fetchall()
 
         # Redirecting logged in users
         for user in users:
@@ -37,9 +87,8 @@ def register():
 def login_processor():
     username = request.form.get('login')
     current_password = request.form.get('current-password')
-    database = sqlite3.connect(DATABASE)
-    c = database.cursor()
-    users = c.execute('SELECT * from Users').fetchall()
+    CURSOR.execute('SELECT * from Users')
+    users = CURSOR.fetchall()
     user_found = False
 
     if not username or not current_password:
@@ -66,9 +115,8 @@ def register_processor():
     new_password = request.form.get('new-password')
     repeat_new_password = request.form.get('repeat-new-password')
     email = request.form.get('email')
-    database = sqlite3.connect(DATABASE)
-    c = database.cursor()
-    users = c.execute('SELECT * from Users').fetchall()
+    CURSOR.execute('SELECT * from Users')
+    users = CURSOR.fetchall()
 
     username_exists = username in [user[0] for user in users]
     email_exists = email in [user[3] for user in users]
@@ -86,9 +134,9 @@ def register_processor():
     else:
         cookie_id = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(255))
         password_hash = pbkdf2_sha512.encrypt(new_password, rounds=200000, salt_size=64)
-        c.execute('INSERT INTO Users (Username, Password, CookieID, Email) VALUES(?, ?, ?, ?)',
-                  (username, password_hash, cookie_id, email))
-        database.commit()
+        CURSOR.execute('INSERT INTO Users (Username, Password, CookieID, Email) VALUES(%s, %s, %s, %s)',
+                       (username, password_hash, cookie_id, email))
+        DATABASE.commit()
         resp = make_response(render_template('success.html', code='register_success'))
         resp.set_cookie('MSTID', cookie_id, max_age=60*60*24*30)
         return resp
@@ -103,9 +151,8 @@ def logout():
 
 @app.route('/account/')
 def account():
-    database = sqlite3.connect(DATABASE)
-    c = database.cursor()
-    users = c.execute('SELECT * from Users').fetchall()
+    CURSOR.execute('SELECT * from Users')
+    users = CURSOR.fetchall()
     for user in users:
         if request.cookies.get('MSTID') == user[2]:
             return render_template('account.html', username=user[0])
@@ -114,9 +161,8 @@ def account():
 
 @app.route('/settings/')
 def settings():
-    database = sqlite3.connect(DATABASE)
-    c = database.cursor()
-    users = c.execute('SELECT * from Users').fetchall()
+    CURSOR.execute('SELECT * from Users')
+    users = CURSOR.fetchall()
     for user in users:
         if request.cookies.get('MSTID') == user[2]:
             return render_template('settings.html')
@@ -130,10 +176,8 @@ def settings_processor():
     repeat_email = request.form.get('repeat-email')
     new_password = request.form.get('new-password')
     repeat_new_password = request.form.get('repeat-new-password')
-
-    database = sqlite3.connect(DATABASE)
-    c = database.cursor()
-    users = c.execute('SELECT * from Users').fetchall()
+    CURSOR.execute('SELECT * from Users')
+    users = CURSOR.fetchall()
 
     new_password_check = new_password == repeat_new_password
     email_check = email == repeat_email
@@ -159,19 +203,19 @@ def settings_processor():
         return render_template('error.html', code='email_exists')
 
     if username:
-        c.execute('UPDATE Users SET Username = ? WHERE ID = ?', (username, user_id))
-        database.commit()
+        CURSOR.execute('UPDATE Users SET Username = %s WHERE ID = %s', (username, user_id))
+        DATABASE.commit()
 
     if email:
-        c.execute('UPDATE Users SET Email = ? WHERE ID = ?', (email, user_id))
-        database.commit()
+        CURSOR.execute('UPDATE Users SET Email = %s WHERE ID = %s', (email, user_id))
+        DATABASE.commit()
 
     if new_password:
         password_hash = pbkdf2_sha512.encrypt(new_password, rounds=200000, salt_size=64)
         cookie_id = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(255))
-        c.execute('UPDATE Users SET Password = ?, CookieID = ? WHERE ID = ?',
-                  (password_hash, cookie_id, user_id))
-        database.commit()
+        CURSOR.execute('UPDATE Users SET Password = %s, CookieID = %s WHERE ID = %s',
+                       (password_hash, cookie_id, user_id))
+        DATABASE.commit()
         resp = make_response(render_template('success.html', code='edit_success'))
         resp.set_cookie('MSTID', cookie_id, max_age=60*60*24*30)
         return resp
@@ -186,9 +230,8 @@ def delete():
 
 @app.route('/delete_confirm/')
 def delete_confirm():
-    database = sqlite3.connect(DATABASE)
-    c = database.cursor()
-    users = c.execute('SELECT * from Users').fetchall()
+    CURSOR.execute('SELECT * from Users')
+    users = CURSOR.fetchall()
     username = None
 
     for user in users:
@@ -199,26 +242,25 @@ def delete_confirm():
     if not username:
         return render_template('error.html', code='logged_out')
 
-    c.execute('DELETE FROM Users WHERE Username = ?', (username,))
-    database.commit()
+    CURSOR.execute('DELETE FROM Users WHERE Username = %s', (username,))
+    DATABASE.commit()
     return redirect('/', code=302)
 
 
 @app.route('/posts/')
 def posts():
-    database = sqlite3.connect(DATABASE)
-    c = database.cursor()
-    blog_posts = c.execute('SELECT * FROM Posts').fetchall()
+    CURSOR.execute('SELECT * FROM Posts ORDER BY Date DESC')
+    blog_posts = CURSOR.fetchall()
     return render_template('posts.html', posts=blog_posts)
 
 
 @app.route('/post/')
 def post():
     post_link = request.args.get('post')
-    database = sqlite3.connect(DATABASE)
-    c = database.cursor()
-    blog_posts = c.execute('SELECT * FROM Posts').fetchall()
-    users = c.execute('SELECT * from Users').fetchall()
+    CURSOR.execute('SELECT * FROM Posts')
+    blog_posts = CURSOR.fetchall()
+    CURSOR.execute('SELECT * from Users')
+    users = CURSOR.fetchall()
     user_id = None
 
     for user in users:
@@ -227,25 +269,27 @@ def post():
 
     for blog_post in blog_posts:
         if blog_post[6] == post_link:
-            is_favourite = bool(c.execute('SELECT * FROM Posts JOIN Favourites WHERE Favourites.Post_ID = Posts.ID '
-                                          'AND Favourites.User_ID = ? AND Post_ID = ?',
-                                          (user_id, blog_post[0])).fetchall())
+            CURSOR.execute('SELECT * FROM Posts JOIN Favourites '
+                           'WHERE Favourites.Post_ID = Posts.ID '
+                           'AND Favourites.User_ID = %s AND Post_ID = %s',
+                           (user_id, blog_post[0]))
+            is_favourite = bool(CURSOR.fetchall())
             return render_template('post.html', theme_color=blog_post[5], post=blog_post, is_favourite=is_favourite)
 
 
 @app.route('/favourites/')
 def favourites():
-    database = sqlite3.connect(DATABASE)
-    c = database.cursor()
-    users = c.execute('SELECT * from Users').fetchall()
+    CURSOR.execute('SELECT * from Users')
+    users = CURSOR.fetchall()
     user_id = None
 
     for user in users:
         if request.cookies.get('MSTID') == user[2]:
             user_id = user[4]
-    blog_posts = c.execute('SELECT * FROM Posts JOIN Favourites '
-                           'WHERE Favourites.Post_ID = Posts.ID and Favourites.User_ID = ?'
-                           'ORDER BY datetime(Favourites.Date_Added) DESC', (user_id,)).fetchall()
+    CURSOR.execute('SELECT * FROM Posts JOIN Favourites '
+                   'WHERE Favourites.Post_ID = Posts.ID and Favourites.User_ID = %s '
+                   'ORDER BY Favourites.Date_Added DESC', (user_id,))
+    blog_posts = CURSOR.fetchall()
     if not user_id:
         return render_template('error.html', code='logged_out')
     if not blog_posts:
@@ -257,42 +301,31 @@ def favourites():
 @app.route('/add_post/')
 def add_post():
     post_id = request.args.get('post')
-    database = sqlite3.connect(DATABASE)
-    c = database.cursor()
-    users = c.execute('SELECT * from Users').fetchall()
+    CURSOR.execute('SELECT * from Users')
+    users = CURSOR.fetchall()
     for user in users:
         if request.cookies.get('MSTID') == user[2]:
             user_id = user[4]
             time = datetime.datetime.now()
-            c.execute('INSERT INTO Favourites(User_ID, Post_ID, Date_Added) VALUES (?, ?, ?)',
-                      (user_id, post_id, time.strftime('%Y-%m-%d %H:%M:%S')))
-            database.commit()
-
+            CURSOR.execute('INSERT INTO Favourites(User_ID, Post_ID, Date_Added) VALUES (%s, %s, %s)',
+                           (user_id, post_id, time.strftime('%Y-%m-%d %H:%M:%S')))
+            DATABASE.commit()
     return "OK"
 
 
 @app.route('/del_post/')
 def del_post():
     post_id = request.args.get('post')
-    database = sqlite3.connect(DATABASE)
-    c = database.cursor()
-    users = c.execute('SELECT * from Users').fetchall()
+    CURSOR.execute('SELECT * from Users')
+    users = CURSOR.fetchall()
     for user in users:
         if request.cookies.get('MSTID') == user[2]:
             user_id = user[4]
-            c.execute('DELETE FROM Favourites WHERE User_ID = ? AND Post_ID = ?',
-                      (user_id, post_id))
-            database.commit()
-
+            CURSOR.execute('DELETE FROM Favourites WHERE User_ID = %s AND Post_ID = %s',
+                           (user_id, post_id))
+            DATABASE.commit()
     return 'OK'
 
 
-def unlock_db():
-    connection = sqlite3.connect(DATABASE)
-    connection.commit()
-    connection.close()
-
-
 if __name__ == '__main__':
-    unlock_db()
     app.run()
