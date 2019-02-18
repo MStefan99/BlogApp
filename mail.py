d4a103e4389973bb3bdfde884f17e83337061417
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2019-02-18 18:16:53 +0200

    moved to local smtp server

--- mail.py
+++ mail.py
@@ -2,23 +2,22 @@ from email_template import get_text
 import email.message
 import smtplib
 
-SMTP_ADDRESS = 'email-smtp.us-west-2.amazonaws.com'
-SMTP_PORT = 587
-SMTP_USERNAME = 'AKIAILKJYPL2KJUEH42Q'
-SMTP_PASSWORD = 'BKodb/aRE+tSJXolb7DVRl4SU7CHqTP0aVeodr6pLDe0'
+EMAIL_ENABLED = True
+SMTP_ADDRESS = '192.168.1.8'
+SMTP_PORT = 3598
 
 
 def send_mail(address, username, link, template):
-    smtp = smtplib.SMTP(SMTP_ADDRESS, SMTP_PORT)
-    smtp.starttls()
-    smtp.login(SMTP_USERNAME, SMTP_PASSWORD)
+    if EMAIL_ENABLED:
+        smtp = smtplib.SMTP(SMTP_ADDRESS, SMTP_PORT)
 
-    msg = email.message.Message()
-    msg['Subject'] = 'galera.dev registration'
-    msg['From'] = 'noreply@mstefan99.com'
-    msg['To'] = address
-    msg.add_header('Content-Type', 'text/html')
-    msg.set_payload(get_text(username, link, template=template))
+        msg = email.message.Message()
+        msg['Subject'] = 'galera.dev registration'
+        msg['From'] = 'noreply@mstefan99.com'
+        msg['To'] = address
+        msg.add_header('Content-Type', 'text/html')
+        msg.set_payload(get_text(username, link, template=template))
 
-    smtp.sendmail(msg['From'], [msg['To']], msg.as_string())
+        smtp.sendmail(msg['From'], [msg['To']], msg.as_string())
+        smtp.close()
 
