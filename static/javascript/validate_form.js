6218766fdc74b23920675cce0a9709fad9b22070
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2019-02-25 19:26:29 +0200

    added description of the script

--- static/javascript/validate_form.js
+++ static/javascript/validate_form.js
@@ -22,7 +22,7 @@ class form {
                 this.submit.classList.add("disabled");
                 this.submit.innerText = "Sending, please wait...";
                 if (this.validate_all()) {
-                    this.submit_form();
+                    this.form_element.submit()
                 } else {
                     this.submit.innerText = "Submit";
                 }
@@ -37,7 +37,7 @@ class form {
                     this.submit.classList.add("disabled");
                     this.submit.innerText = "Sending, please wait...";
                     if (this.validate_all()) {
-                        this.submit_form();
+                        this.form_element.submit()
                     } else {
                         this.submit.innerText = "Submit";
                     }
@@ -56,7 +56,7 @@ class form {
         this.new_password = this.form_element.querySelector("input.new-password");
         this.repeat_new_password = this.form_element.querySelector("input.repeat-new-password");
         this.required = this.form_element.querySelectorAll("input[required]");
-        this.submit = this.form_element.querySelector("button.smart-submit");
+        this.submit = this.form_element.querySelector("button");
 
 
 // Feedback elements
@@ -76,9 +76,11 @@ class form {
                     this.email.addEventListener(event, () => {
                         this.check_email(this.email, this.email_msg);
                     });
-                    this.email.addEventListener(event, () => {
-                        this.check_email_match(this.email, this.repeat_email, this.repeat_email_msg);
-                    });
+                    if (this.repeat_email) {
+                        this.email.addEventListener(event, () => {
+                            this.check_email_match(this.email, this.repeat_email, this.repeat_email_msg);
+                        });
+                    }
                 });
             }
         } else {
@@ -137,7 +139,7 @@ class form {
         if (this.repeat_new_password) {
             if (this.repeat_new_password.classList.contains("realtime-validate")) {
                 ["keyup", "paste", "load"].forEach((event) => {
-                    this.repeat_new_password.addEventListener("keyup", () => {
+                    this.repeat_new_password.addEventListener(event, () => {
                         this.check_password_match(this.new_password,
                             this.repeat_new_password, this.repeat_new_password_msg);
                     });
@@ -151,19 +153,18 @@ class form {
 
 
 //Check functions
-    check_username(element, element_to_set, async = true) {
-        element.value = element.value.trim().replace(/\s+/g, '');
-        if (!element.value.trim()) {
-            element_to_set.innerHTML = "";
-            this.username_ok = !element.hasAttribute('required');
-            this.validate_form();
+    check_username(username, username_msg, async = true) {
+        username.value = username.value.trim().replace(/\s+/g, '');
+        if (!username.value.trim()) {
+            username_msg.innerHTML = "";
+            this.username_ok = !username.hasAttribute('required');
         } else {
             let xhr = new XMLHttpRequest();
             xhr.onreadystatechange = () => {
                 if (xhr.readyState === 4 && xhr.status === 200) {
                     let response = xhr.responseText.split(";", 2);
-                    element_to_set.className = "credentials-check " + response[0];
-                    element_to_set.innerHTML = response[1];
+                    username_msg.className = "credentials-check " + response[0];
+                    username_msg.innerHTML = response[1];
                     this.username_ok = response[0] === "ok";
                     this.validate_form();
                 }
@@ -171,24 +172,24 @@ class form {
 
             xhr.open("POST", "/check_username/", async);
             xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhr.send("username=" + element.value);
+            xhr.send("username=" + username.value);
         }
+        this.validate_form();
     }
 
 
-    check_login(element, element_to_set, async = true) {
-        element.value = element.value.trim().replace(/\s+/g, '');
-        if (!element.value.trim()) {
-            element_to_set.innerHTML = "";
-            this.login_ok = !element.hasAttribute('required');
-            this.validate_form();
+    check_login(login, login_msg, async = true) {
+        login.value = login.value.trim().replace(/\s+/g, '');
+        if (!login.value.trim()) {
+            login_msg.innerHTML = "";
+            this.login_ok = !login.hasAttribute('required');
         } else {
             let xhr = new XMLHttpRequest();
             xhr.onreadystatechange = () => {
                 if (xhr.readyState === 4 && xhr.status === 200) {
                     let response = xhr.responseText.split(";", 2);
-                    element_to_set.className = "credentials-check " + response[0];
-                    element_to_set.innerHTML = response[1];
+                    login_msg.className = "credentials-check " + response[0];
+                    login_msg.innerHTML = response[1];
                     this.login_ok = response[0] === "ok";
                     this.validate_form();
                 }
@@ -196,92 +197,82 @@ class form {
 
             xhr.open("POST", "/check_login/", async);
             xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhr.send("login=" + element.value);
+            xhr.send("login=" + login.value);
         }
+        this.validate_form();
     }
 
 
-    check_email(element, element_to_set, async = true) {
-        element.value = element.value.trim().replace(/\s+/g, '');
-        if (!element.value) {
-            element_to_set.innerHTML = "";
-            this.email_ok = !element.hasAttribute('required');
-            this.validate_form();
+    check_email(email, email_msg, async = true) {
+        email.value = email.value.trim().replace(/\s+/g, '');
+        if (!email.value) {
+            email_msg.innerHTML = "";
+            this.email_ok = !email.hasAttribute('required');
         } else {
             let xhr = new XMLHttpRequest();
             xhr.onreadystatechange = () => {
                 if (xhr.readyState === 4 && xhr.status === 200) {
                     let response = xhr.responseText.split(";", 2);
-                    element_to_set.className = "credentials-check " + response[0];
-                    element_to_set.innerHTML = response[1];
+                    email_msg.className = "credentials-check " + response[0];
+                    email_msg.innerHTML = response[1];
                     this.email_ok = response[0] === "ok";
                     this.validate_form();
                 }
             };
             xhr.open("POST", "/check_email/", async);
             xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhr.send("email=" + element.value);
+            xhr.send("email=" + email.value);
         }
+        this.validate_form();
     }
 
 
-    check_email_match(element1, element2, element_to_set) {
-        if (!element1.value && element1.hasAttribute('required') &&
-            !element2.value && element2.hasAttribute('required')) {
+    check_email_match(email, email_repeat, email_match_msg) {
+        if (!email.value && email.hasAttribute('required') &&
+            !email_repeat.value && email_repeat.hasAttribute('required')) {
             this.email_match_ok = false;
-        } else if (element1.value !== element2.value) {
-            element_to_set.innerHTML = "Emails do not match";
-            element_to_set.className = "credentials-check error";
+        } else if (email.value !== email_repeat.value) {
+            email_match_msg.innerHTML = "Emails do not match";
+            email_match_msg.className = "credentials-check error";
             this.email_match_ok = false;
         } else {
-            element_to_set.innerHTML = "";
+            email_match_msg.innerHTML = "";
             this.email_match_ok = true;
         }
         this.validate_form();
     }
 
 
-    check_password_match(element1, element2, element_to_set) {
-        if (!element1.value && element1.hasAttribute('required') &&
-            !element2.value && element2.hasAttribute('required')) {
-            element_to_set.innerHTML = "";
+    check_password_match(password, password_repeat, password_msg) {
+        if (!password.value && password.hasAttribute('required') &&
+            !password_repeat.value && password_repeat.hasAttribute('required')) {
+            password_msg.innerHTML = "";
             this.password_match_ok = false;
-        } else if (!element1.value.trim() && !element2.value.trim()) {
-            element_to_set.innerHTML = "";
-        } else if (element1.value !== element2.value) {
-            element_to_set.innerHTML = "Passwords do not match";
-            element_to_set.className = "credentials-check error";
+        } else if (!password.value.trim() && !password_repeat.value.trim()) {
+            password_msg.innerHTML = "";
+        } else if (password.value !== password_repeat.value) {
+            password_msg.innerHTML = "Passwords do not match";
+            password_msg.className = "credentials-check error";
             this.password_match_ok = false;
-        } else if (element1.value === element2.value) {
-            element_to_set.innerHTML = "Passwords match";
-            element_to_set.className = "credentials-check ok";
+        } else if (password.value === password_repeat.value) {
+            password_msg.innerHTML = "Passwords match";
+            password_msg.className = "credentials-check ok";
             this.password_match_ok = true;
         }
         this.validate_form();
     }
 
-    check_required(element_to_set) {
+    check_required(required_msg) {
         this.required_ok = true;
-        element_to_set.innerHTML = "";
+        required_msg.innerHTML = "";
         Array.from(this.required).forEach((i) => {
             if (!i.value) {
                 this.required_ok = false;
-                element_to_set.innerHTML = "Please fill in all required fields";
-                element_to_set.className = "credentials-check error";
+                required_msg.innerHTML = "Please fill in all required fields";
+                required_msg.className = "credentials-check error";
             }
-        })
-    }
-
-
-// Enabling or disabling form submission
-    validate_form() {
-        if (!this.email_ok || !this.email_match_ok || !this.username_ok || !this.password_match_ok) {
-            this.submit.classList.add("disabled");
-            return false;
-        } else {
-            this.submit.classList.remove("disabled");
-            return true;
-        }
+        });
+        this.validate_form();
     }
 
 
@@ -304,16 +295,27 @@ class form {
         if (this.required) {
             this.check_required(this.required_msg);
         }
-        return this.email_ok && this.email_match_ok && this.username_ok && this.login_ok && this.password_match_ok && this.required_ok;
+        // Enabling or disabling form submission
+        var ok = this.email_ok && this.email_match_ok && this.username_ok && this.login_ok && this.password_match_ok && this.required_ok;
+        if (!ok) {
+            this.submit.classList.add("disabled")
+        } else {
+            this.submit.classList.remove("disabled");
+        }
+        return ok;
     }
 
 
-    submit_form() {
-        if (this.validate_all()) {
-            this.form_element.submit()
+    // Enabling or disabling form submission
+    validate_form() {
+        if (this.email_ok && this.email_match_ok && this.username_ok && this.login_ok && this.password_match_ok && this.required_ok) {
+            this.submit.classList.remove("disabled");
+            return false;
+        } else {
+            this.submit.classList.add("disabled");
+            return true;
         }
     }
-
 }
 
 
