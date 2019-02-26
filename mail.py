1180c15ac6805f01383961a3c7baec0038d59d23
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2019-02-26 14:23:58 +0200

    added error pages

--- mail.py
+++ mail.py
@@ -2,7 +2,7 @@ from email_template import get_email_text
 from mailer import *
 
 # WARNING! Disabling email is used only during development process! ALWAYS TURN EMAIL ON BEFORE DEPLOYMENT!
-EMAIL_ENABLED = True  
+EMAIL_ENABLED = True
 SMTP_ADDRESS = '192.168.1.8'
 SMTP_PORT = 3598
 
