049144f6a0f668bd1e6efae49c557abf55f41f87
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2019-03-11 19:49:40 +0200

    fixed error message syntax

--- blog/api/v0_1/error.py
+++ blog/api/v0_1/error.py
@@ -3,11 +3,6 @@ from flask import Flask, make_response
 app = Flask(__name__)
 
 
-@app.errorhandler(404)
-def api_page_not_found(error):
-    return make_response('PAGE NOT FOUND', 404)
-
-
 @app.errorhandler(500)
 def api_internal_error(error):
     return make_response('INTERNAL ERROR', 500)
