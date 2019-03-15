799d7624b68705500d5254f305c4d9a8670d03a2
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2019-03-15 18:27:14 +0200

    made style with less

--- static/javascript/validate_form.js
+++ static/javascript/validate_form.js
@@ -16,11 +16,11 @@ class form {
     static set_color(ok, ...elements) {
         elements.forEach((element) => {
             if (!element.value) {
-                element.style.borderColor = "var(--accent-color)";
+                element.style.borderColor = "var(--color-accent)";
             } else if (ok) {
-                element.style.borderColor = "var(--ok)";
+                element.style.borderColor = "var(--color-ok)";
             } else {
-                element.style.borderColor = "var(--error)";
+                element.style.borderColor = "var(--color-error)";
             }
         });
 
@@ -299,7 +299,7 @@ class form {
             this.email_match_ok = true;
         }
         this.validate_form();
-        form.set_color(this.email_match_ok, email, email_repeat);
+        form.set_color(this.email_match_ok, email_repeat);
     }
 
     check_password(password, password_msg) {
