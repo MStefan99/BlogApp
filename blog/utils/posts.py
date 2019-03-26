1415800a7416eb69e82775e267ec85cf1bac400f
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2019-03-26 22:18:55 +0200

    added search, combined posts pages

--- blog/utils/posts.py
+++ blog/utils/posts.py
@@ -7,14 +7,16 @@ from blog.utils.users import DATABASE
 
 def get_posts():
     cursor = DATABASE.cursor()
-    cursor.execute('select * from posts order by date desc')
+    cursor.execute('select * from posts '
+                   'order by date desc')
     return cursor.fetchall()
 
 
 def get_favourites(user):
     cursor = DATABASE.cursor()
     cursor.execute('select posts.* from posts join favourites '
-                   'on (favourites.post_id = posts.id and favourites.user_id = %s) '
+                   'on (favourites.post_id = posts.id '
+                   'and favourites.user_id = %s) '
                    'order by favourites.date_added desc', (user['id'],))
     return cursor.fetchall()
 
@@ -24,7 +26,8 @@ def check_favourite(user, post):
     if user and post:
         cursor.execute('select * from posts join favourites '
                        'on (favourites.post_id = posts.id '
-                       'and favourites.user_id = %s and post_id = %s)',
+                       'and favourites.user_id = %s '
+                       'and post_id = %s)',
                        (user['id'], post['id']))
         return bool(cursor.fetchall())
 
@@ -33,7 +36,8 @@ def save_post(user, post):
     cursor = DATABASE.cursor()
     time = datetime.now()
     try:
-        cursor.execute('insert into favourites(user_id, post_id, date_added) values (%s, %s, %s)',
+        cursor.execute('insert into favourites(user_id, post_id, date_added) '
+                       'values (%s, %s, %s)',
                        (user['id'], post['id'], time.strftime('%Y-%m-%d %H:%M:%S')))
     except psycopg2.IntegrityError:
         return 'ALREADY EXISTS'
@@ -42,6 +46,20 @@ def save_post(user, post):
 
 def remove_post(user, post):
     cursor = DATABASE.cursor()
-    cursor.execute('delete from favourites where user_id = %s and post_id = %s',
+    cursor.execute('delete from favourites '
+                   'where user_id = %s '
+                   'and post_id = %s',
                    (user['id'], post['id']))
     return 'OK'
+
+
+def search_posts_by_text(query):
+    query = '%' + query + '%'
+    cursor = DATABASE.cursor()
+    cursor.execute('select * from posts '
+                   'where content like %s '
+                   'or title like %s '
+                   'or tagline like %s '
+                   'order by date desc',
+                   (query, query, query))
+    return cursor.fetchall()
--- blog/web/web.py
+++ blog/web/web.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, make_response
 
 from blog.globals import COOKIE_NAME
-from blog.utils.posts import get_posts, check_favourite, get_favourites
+from blog.utils.posts import get_posts, check_favourite, get_favourites, search_posts_by_text
 from blog.utils.search import find_user_by_cookie, find_post_by_link
 from blog.utils.users import get_user
 from blog_app import app
@@ -111,7 +111,7 @@ def web_favourites():
 
     posts = get_favourites(user)
     if not posts:
-        return render_template('posts/favourites.html', code='no_posts')
+        return render_template('posts/posts.html', code='no_posts')
     current_page = request.args.get('page')
 
     if not current_page:
@@ -123,7 +123,28 @@ def web_favourites():
     if len(posts) > 10:
         posts = posts[current_page * 10:current_page * 10 + 10]
 
-    return render_template('posts/favourites.html', posts=posts, current_page=current_page, pages_number=pages_number)
+    return render_template('posts/posts.html', posts=posts, current_page=current_page, pages_number=pages_number)
+
+
+@app.route('/search/')
+def web_search():
+    query = request.args.get('q')
+    posts = search_posts_by_text(query)
+
+    if not posts:
+        return render_template('posts/posts.html', code='no_posts')
+    current_page = request.args.get('page')
+
+    if not current_page:
+        current_page = 0
+    else:
+        current_page = int(current_page)
+    pages_number = len(posts) // 10 + 1
+
+    if len(posts) > 10:
+        posts = posts[current_page * 10:current_page * 10 + 10]
+
+    return render_template('posts/posts.html', posts=posts, current_page=current_page, pages_number=pages_number)
 
 
 @app.route('/secret/')
